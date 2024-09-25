cd2f7e5a6453112a68b620ac8db00d8789e5ebc5
SonnySon1 <sonysudrajat94@gmail.com>
2024-09-25 10:39:25 +0700

    menambahkan validate di store siswa

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,28 @@ class UserController extends Controller
 
 
     public  function store(Request $request) {
+
+
+        // validasi data
+        $request->validate([
+            'name'       =>  'required|max:255',
+            'nis'        =>  'required|max:8|unique:users,nis',           
+            'kelas'      =>  'required',           
+            'jurusan_id' =>  'required',           
+            'password'   =>  'required|min:8'           
+        ],[
+            'name.required'       => 'Nama harus diisi',
+            'name.max'            => 'Nama maksimal 255 karakter',
+            'nis.required'        => 'Nis harus diisi',
+            'nis.max'             => 'Nis maksimal 8 karakter',
+            'nis.unique'          => 'Nis sudah ada',
+            'kelas.required'      => 'Kelas harus diisi',
+            'jurusan_id.required' => 'Jurusan harus diisi',
+            'password.required'   => 'Password harus diisi',
+            'password.min'        => 'Password minimal 8 karakter',
+        ]);
+
+
         // masukan data ke database crud_sekolah nama table siswa
         $storeDataSiswa = [
             'nama'       => $request->name,
--- resources/views/siswa/create.blade.php
+++ resources/views/siswa/create.blade.php
@@ -6,28 +6,43 @@
         @csrf
         <div class="mb-3">
             <label for="">Nama</label>
-            <input name="name" type="text" class="form-control" placeholder="Nama" aria-label="Username" aria-describedby="basic-addon1">
+            <input name="name" value="{{ old('name') }}" type="text" class="form-control" placeholder="Nama" aria-label="Username" aria-describedby="basic-addon1">
+            @error('name')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>          
         <div class="mb-3">
             <label for="">Nis</label>
-            <input name="nis" type="text" class="form-control" placeholder="Nis" aria-label="Username" aria-describedby="basic-addon1">
+            <input name="nis" value="{{ old('nis') }}"  type="text" class="form-control" placeholder="Nis" aria-label="Username" aria-describedby="basic-addon1">
+            @error('nis')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>          
         <div class="mb-3">
             <label for="">Kelas</label>
-            <input name="kelas" type="text" class="form-control" placeholder="Kelas" aria-label="Username" aria-describedby="basic-addon1">
+            <input name="kelas" value="{{ old('kelas') }}"  type="text" class="form-control" placeholder="Kelas" aria-label="Username" aria-describedby="basic-addon1">
+            @error('kelas')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>          
         <div class="mb-3">
             <label for="">Jurusan</label>
             <select name="jurusan_id" class="form-select" aria-label="Default select example">
-                <option selected>--Pilih Jurusan--</option>
+                <option value="" selected>--Pilih Jurusan--</option>
                 @foreach ($jurusans as $jurusan)
-                    <option value="{{ $jurusan->id }}">{{ $jurusan->nama_jurusan }}</option>
+                    <option {{ old('jurusan_id') == $jurusan->id ? 'selected' : '' }} value="{{ $jurusan->id }}">{{ $jurusan->nama_jurusan }}</option>
                 @endforeach
               </select>
+              @error('jurusan_id')
+                    <p class="text-danger">{{ $message }}</p>
+                @enderror
         </div>          
-        <div class="mb-3">
+        <div class="mb-3 d-block">
             <label for="">Password</label>
-            <input name="password" type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
+            <input  name="password" value="{{ old('password') }}"  type="text" class="form-control " placeholder="Password" aria-label="Username" aria-describedby="basic-addon1">
+            @error('password')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>          
         <div>
             <button type="submit">Tambah</button>
